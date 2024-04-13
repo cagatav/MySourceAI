48e56aef83e0091c8549427f1e764b44c88d4235
cagatav <112089960+cagatav@users.noreply.github.com>
2024-04-13 21:01:08 +0300

    small update

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -5,11 +5,9 @@ export default async function handler(req, res) {
 
   if (req.method !== 'POST') {
     res.status(405).json({ message: 'Method should be POST' });
-    toast.error('Method should be POST');
   } else if (process.env.NODE_ENV !== "development") {
     if (!referer || referer !== process.env.APP_URL) {
       res.status(401).json({ message: 'Unauthorized' });
-      toast.error('Unauthorized');
     }
   }
   else {
@@ -27,8 +25,7 @@ export default async function handler(req, res) {
     } catch (error) {
       console.log(error);
       res.status(500).json({ message: "Something went wrong" });
-      toast.error('Something went wrong');
     }
   }
-  
+   
 }
