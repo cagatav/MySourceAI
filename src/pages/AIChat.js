c5b550c072b796314550e20a459efc77ebea0b0a
cagatav <cgty.szn@gmail.com>
2024-04-15 16:39:11 +0300

    tst

--- src/pages/AIChat.js
+++ src/pages/AIChat.js
@@ -13,7 +13,7 @@ export default function AIChat() {
     const [isLoading, setIsLoading] = useState(false);
     const [chatType, setChatType] = useState('GPT 3.5');
     const [temperature, setTemperature] = useState(0.5);
-    const chatBoxRef = useRef(null);  // Mesaj kutusu için ref
+    const chatBoxRef = useRef(null);
 
     useEffect(() => {
         const storedChatLog = JSON.parse(localStorage.getItem('chatLog')) || [];
@@ -26,7 +26,6 @@ export default function AIChat() {
     }, []);
 
     useEffect(() => {
-        // Yalnızca mesaj kutusunun altına kaydırma
         if(chatBoxRef.current) {
             chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
         }
@@ -58,6 +57,10 @@ export default function AIChat() {
             temperature: temperature,
             max_tokens: 200
         };
+
+        console.log("Sending the following data to the API:", data);
+        console.log("Base Prompt:", basePrompt);
+        console.log("Dialog History:", userMessage);
 
         setIsLoading(true);
         axios.post(url, data).then((response) => {
@@ -96,7 +99,7 @@ export default function AIChat() {
                         <button
                             onClick={clearHistory}
                             style={{ position: 'absolute', right: '20px' }}
-                            className="bg-transparent hover:opacity-100 opacity-70 text-gray-700 font-semibold py-2 px-4 rounded">
+                            className="bg-transparent hover:opacity-100 opacity-70">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 24 24">
                                 <path d="M20.944 12.979c-.489 4.509-4.306 8.021-8.944 8.021-2.698 0-5.112-1.194-6.763-3.075l1.245-1.633c1.283 1.645 3.276 2.708 5.518 2.708 3.526 0 6.444-2.624 6.923-6.021h-2.923l4-5.25 4 5.25h-3.056zm-15.864-1.979c.487-3.387 3.4-6 6.92-6 2.237 0 4.228 1.059 5.51 2.698l1.244-1.632c-1.65-1.876-4.061-3.066-6.754-3.066-4.632 0-8.443 3.501-8.941 8h-3.059l4 5.25 4-5.25h-2.92z"/>
                             </svg>
