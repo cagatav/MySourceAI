f61dc65541bcea404bfbcfb2eb1816bb91930c00
cagatav <cgty.szn@gmail.com>
2024-04-19 14:27:13 +0300

    bi şey test ediyom

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -1,16 +1,9 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
-  const referer = req.headers.referer || req.headers.referrer;
-
   if (req.method !== 'POST') {
     res.status(405).json({ message: 'Method should be POST' });
-  } else if (process.env.NODE_ENV !== "development") {
-    if (!referer || referer !== process.env.APP_URL) {
-      res.status(401).json({ message: 'Unauthorized' });
-    }
-  }
-  else {
+  } else {
     try {
       const { body } = req;
       const url = 'https://api.openai.com/v1/chat/completions';
@@ -19,13 +12,11 @@ export default async function handler(req, res) {
         'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
       };
 
-      const response = await axios.post(url, body, { headers: headers })
-
+      const response = await axios.post(url, body, { headers: headers });
       res.status(200).json(response.data);
     } catch (error) {
       console.log(error);
       res.status(500).json({ message: "Something went wrong" });
     }
   }
-   
 }
