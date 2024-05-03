b36a9169929099d68ab7844e86bee71126e1c853
cagatav <cgty.szn@gmail.com>
2024-05-03 20:03:05 +0300

    localstorage infinite fix

--- src/pages/Sources.js
+++ src/pages/Sources.js
@@ -17,9 +17,8 @@ export default function Sources() {
       setTextItems(JSON.parse(storedTextItems));
     } else {
       localStorage.setItem('textItems', JSON.stringify([]));
-      setTextItems([]);
     }
-  }, [textItems]); 
+  }, []); 
 
   const autoResize = () => {
     const element = textAreaRef.current;
@@ -295,7 +294,7 @@ export default function Sources() {
     )}
     <button onClick={() => handleDeleteText(item.id)} className="ml-5 duration-200 bg-transparent text-white py-1 px-1 rounded-lg border border-transparent hover:border-white hover:opacity-75"
             style={{ height: 'auto', wordBreak: "keep-all"}}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 16 16"><path fill="currentColor" fill-rule="evenodd" d="M10 3h3v1h-1v9l-1 1H4l-1-1V4H2V3h3V2a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1zM9 2H6v1h3zM4 13h7V4H4zm2-8H5v7h1zm1 0h1v7H7zm2 0h1v7H9z"/></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 16 16"><path fill="currentColor" d="M10 3h3v1h-1v9l-1 1H4l-1-1V4H2V3h3V2a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1zM9 2H6v1h3zM4 13h7V4H4zm2-8H5v7h1zm1 0h1v7H7zm2 0h1v7H9z"/></svg>
     </button>
   </div>
 ))} 
